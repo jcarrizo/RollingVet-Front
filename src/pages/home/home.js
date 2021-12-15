648d372fe05e63504608ce4a859be51d5992d23a
jcarrizo <jcarrizo15@gmail.com>
2021-12-15 20:54:57 -0300

    creacopn del Hero

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Navbar from '../../components/navbar/navbar';
 import '../home/home.css';
+import hero from "../../images/home/hero2.png"
+
 
 function Home() {
   return (
-    <div>
+    <div className='body'>
       <Navbar />
-    </div>
+      <section>
+        <img src={hero} className="d-block w-100 hero-img" alt="..."></img>
+        <div className='hero-text'>
+          <h2>We are what</h2>
+          <h2>your dog needs</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the r</p>
+          <button type="button" className="btn btn-primary">Primary</button>
+        </div>
+      </section>
+      <section>
+
+      </section>
+
+    </div >
   );
 }
 
