fa1bbcaffdee7b21d4f4912b88ddb8732f7fd0ad
jcarrizo <jcarrizo15@gmail.com>
2021-12-15 17:54:00 -0300

    creacion del navbar

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,31 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/navbar/navbar';
 import '../home/home.css';
 
 function Home() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar w/ text</a>
-                    <div className="collapse navbar-collapse" id="navbarText">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                        </ul>
-                        <span className="navbar-text">
-                            Navbar text with an inline element
-                        </span>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default Home;
