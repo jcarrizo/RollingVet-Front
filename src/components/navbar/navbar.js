20f4d5cf67c05234847ef4c600c274db3a9df6d2
jcarrizo <jcarrizo15@gmail.com>
2021-12-15 18:26:58 -0300

    implementacion de icon

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../navbar/navbar.css';
+import icon from "../../images/home/vet-icon.png"
 
 function Navbar() {
   return (
-    <div>
-      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <div >
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom body-header">
         <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-          <p>Hola</p>
+          <img src={icon} className="logo"></img>
         </a>
-
         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
           <li><a href="#" className="nav-link px-2 link-dark">Features</a></li>
@@ -16,7 +16,6 @@ function Navbar() {
           <li><a href="#" className="nav-link px-2 link-dark">FAQs</a></li>
           <li><a href="#" className="nav-link px-2 link-dark">About</a></li>
         </ul>
-
         <div className="col-md-3 text-end">
           <button type="button" className="btn btn-outline-primary me-2">Login</button>
         </div>
