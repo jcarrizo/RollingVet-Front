d10e7787f452ffb87057f12ad2c84db98ba8106d
jcarrizo <jcarrizo15@gmail.com>
2021-12-16 00:15:35 -0300

    creacion de servicios

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Navbar from '../../components/navbar/navbar';
 import '../home/home.css';
-import hero from "../../images/home/hero2.png"
-
+import hero from "../../images/home/hero.png"
+import veterinaria from "../../images/home/veterinaria.png"
+import revision from "../../images/home/01-revision.png"
+import asistencia from "../../images/home/02-asistencia.png"
+import vacunacion from "../../images/home/03-vacuanacion.png"
+import desparasitacion from "../../images/home/04-desparasitacion.png"
+import certificado from "../../images/home/05-certificado.png"
+import cirujia from "../../images/home/06-cirujia.png"
+import internacion from "../../images/home/07-internacion.png"
+import laboratorio from "../../images/home/08-laboratory.png"
 
 function Home() {
   return (
@@ -17,8 +25,69 @@ function Home() {
           <button type="button" className="btn btn-primary">Primary</button>
         </div>
       </section>
-      <section>
 
+      {/* Seccion Servicios */}
+      <section className='section-servicios container'>
+        <h3> Servicios para tu mascota</h3>
+        <div className='row'>
+          <div className='col-md-6 d-flex justify-content-center align-items-center'>
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <div className='row'>
+                    <div className='col-md-6 d-flex flex-column justify-content-center align-items-center mb-5'>
+                      <img src={revision} className='icon-servicios'></img>
+                      <h4>Revision General</h4>
+                    </div>
+                    <div className='col-md-6 d-flex flex-column justify-content-center align-items-center mb-5'>
+                      <img src={asistencia} className='icon-servicios'></img>
+                      <h4>Asistencia Integral</h4>
+                    </div>
+                    <div className='col-md-6 d-flex flex-column justify-content-center align-items-center'>
+                      <img src={vacunacion} className='icon-servicios'></img>
+                      <h4>Vacunación</h4>
+                    </div>
+                    <div className='col-md-6 d-flex flex-column justify-content-center align-items-center'>
+                      <img src={desparasitacion} className='icon-servicios'></img>
+                      <h4>Desparasitación</h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <div className='row'>
+                    <div className='col-md-6 d-flex flex-column justify-content-center align-items-center mb-5'>
+                      <img src={certificado} className='icon-servicios'></img>
+                      <h4>Certificado</h4>
+                    </div>
+                    <div className='col-md-6 d-flex flex-column justify-content-center align-items-center mb-5'>
+                      <img src={cirujia} className='icon-servicios'></img>
+                      <h4>Cirujia</h4>
+                    </div>
+                    <div className='col-md-6 d-flex flex-column justify-content-center align-items-center'>
+                      <img src={internacion} className='icon-servicios'></img>
+                      <h4>Internacion</h4>
+                    </div>
+                    <div className='col-md-6 d-flex flex-column justify-content-center align-items-center'>
+                      <img src={laboratorio} className='icon-servicios'></img>
+                      <h4>Laboratorio</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+          <div className='col-md-6 d-flex justify-content-center'>
+            <img src={veterinaria} className="d-block img-servicios"></img>
+          </div>
+        </div>
       </section>
 
     </div >
