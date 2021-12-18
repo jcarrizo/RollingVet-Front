d564eb354c3c5e7b94a849269dea73b56db96d31
jcarrizo <jcarrizo15@gmail.com>
2021-12-18 01:34:52 -0300

    creacion del login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../login/login.css'
 
 const Login = () => {
   return (
-    <div>
-      <p>Hola</p>
+    <div className="main">
+      <p className="sign" align="center">Sign in</p>
+      <form className="form1">
+        <input className="un " type="text" align="center" placeholder="Username"></input>
+        <input className="pass" type="password" align="center" placeholder="Password"></input>
+        <a className="submit" align="center">Sign in</a>
+      </form >
     </div>
   )
 }
