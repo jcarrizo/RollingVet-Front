00b9d97baae527a7d6920869593a6b16aacf8981
jcarrizo <jcarrizo15@gmail.com>
2021-12-18 11:04:28 -0300

    edicion de Login

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './pages/home/home';
 import Rutas from './routes/rutas';
+import "../src/index.css"
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import '../login/login.css'
+import Logo from '../../images/home/vet-icon.png'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div className="main">
-      <p className="sign" align="center">Sign in</p>
+      <Link to="/">
+        <img className='logo-login d-flex align-items-center' src={Logo}></img>
+      </Link>
       <form className="form1">
-        <input className="un " type="text" align="center" placeholder="Username"></input>
-        <input className="pass" type="password" align="center" placeholder="Password"></input>
-        <a className="submit" align="center">Sign in</a>
+        <input className="un " type="text" align="center" placeholder="Correo"></input>
+        <input className="pass" type="password" align="center" placeholder="Contraseña"></input>
+        <a className="submit">Ingresar</a>
       </form >
     </div>
   )
