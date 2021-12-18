5297a68f454fd646d9a23aacdfca8960d9250172
jcarrizo <jcarrizo15@gmail.com>
2021-12-18 18:28:49 -0300

    funcionamiento login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,22 +2,21 @@ import React from 'react'
 import '../login/login.css'
 import Logo from '../../images/home/vet-icon.png'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 const Login = () => {
-
-
-
-
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
 
   return (
     <div className="main">
       <Link to="/">
         <img className='logo-login d-flex align-items-center' src={Logo}></img>
       </Link>
-      <form className="form1">
-        <input className="un " type="text" align="center" placeholder="Correo"></input>
-        <input className="pass" type="password" align="center" placeholder="Contraseña"></input>
-        <a className="submit">Ingresar</a>
+      <form className="form1" onSubmit={handleSubmit(onSubmit)}>
+        <input className="un " type="text" align="center" placeholder="Correo" maxLength={15} {...register("correo", { required: true })} required></input>
+        <input className="pass" type="password" align="center" placeholder="Contraseña" maxLength={15} {...register("contrasena", { required: true })} required></input>
+        <button className="submit" type='submit'>Ingresar</button>
       </form >
     </div>
   )
