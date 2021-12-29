c9014dcb3e921a30e83fb7fe5371d4931db3715c
jcarrizo <jcarrizo15@gmail.com>
2021-12-29 00:00:24 -0300

    cambios en el dashboard

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -5,8 +5,65 @@ import '../sidebar/sidebar.css'
 const Sidebar = () => {
 
   return (
-    <div>
-      <a>hola1</a>
+    <div className="navigation">
+      <ul>
+        <li>
+          <a href="#">
+            <span className="icon">
+              <ion-icon name="person-circle-outline"></ion-icon>
+            </span>
+            <span className="title">User Name</span>
+          </a>
+        </li>
+        <li className='active'>
+          <a href="#">
+            <span className="icon">
+              <ion-icon name="bar-chart-outline"></ion-icon>
+            </span>
+            <span className="title">Estadisticas</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="icon">
+              <ion-icon name="person-add-outline"></ion-icon>
+            </span>
+            <span className="title">Usuarios</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="icon">
+              <ion-icon name="bag-add-outline"></ion-icon>
+            </span>
+            <span className="title">Compras</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="icon">
+              <ion-icon name="cash-outline"></ion-icon>
+            </span>
+            <span className="title">Ventas</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="icon">
+              <ion-icon name="people-outline"></ion-icon>
+            </span>
+            <span className="title">Clientes</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className='mt-5'>
+            <span className="icon">
+              <ion-icon name="log-out-outline"></ion-icon>
+            </span>
+            <span className="title">Sign Out</span>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/pages/dashboard/estadisticas/estadisticas.js
+++ src/pages/dashboard/estadisticas/estadisticas.js
@@ -5,9 +5,56 @@ import '../estadisticas/estadisticas.css'
 const Estadisticas = () => {
 
   return (
-    <div className='container'>
+    <div>
       <Sidebar />
-      <a>hola</a>
+      <div className="maindash">
+        <div className="topbar">
+          <div className="toggle">
+            <ion-icon name="menu-outline"></ion-icon>
+          </div>
+          <div className="user">
+            <img src="./img/user.jpeg" alt=""></img>
+          </div>
+        </div>
+        <div className="cardBox">
+          <div className="card">
+            <div>
+              <div className="numbers">1504</div>
+              <div className="cardName">Daily Views</div>
+            </div>
+            <div className="iconBx">
+              <ion-icon name="eye-outline"></ion-icon>
+            </div>
+          </div>
+          <div className="card">
+            <div>
+              <div className="numbers">80</div>
+              <div className="cardName">Sales</div>
+            </div>
+            <div className="iconBx">
+              <ion-icon name="cart-outline"></ion-icon>
+            </div>
+          </div>
+          <div className="card">
+            <div>
+              <div className="numbers">284</div>
+              <div className="cardName">Comments</div>
+            </div>
+            <div className="iconBx">
+              <ion-icon name="chatbubbles-outline"></ion-icon>
+            </div>
+          </div>
+          <div className="card">
+            <div>
+              <div className="numbers">$7842</div>
+              <div className="cardName">Earning</div>
+            </div>
+            <div className="iconBx">
+              <ion-icon name="cash-outline"></ion-icon>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
