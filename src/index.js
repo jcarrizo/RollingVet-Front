64713c1a6cff7361a100c6eff1168ba3af476393
jcarrizo <jcarrizo15@gmail.com>
2022-02-01 17:29:50 -0300

    creacion abm nuevo usuario

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Rutas from './routes/rutas';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "../src/index.css"
 
 ReactDOM.render(
   <React.StrictMode>
     <Rutas />
+    <ToastContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/dashboard/registroUser/registroUser.js
+++ src/pages/dashboard/registroUser/registroUser.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 import Sidebar from '../../../components/sidebar/sidebar';
 import '../registroUser/registroUser.css'
 
 const RegistroUser = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    console.log(data)
+    fetch('http://localhost:8000/api/user', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        toast("Se cargo el perfil correctamente", {
+          type: "success",
+          autoClose: 3000,
+        });
+      })
+      .catch(err => console.error(err))
+  };
   return (
     <div>
       <Sidebar />
@@ -11,8 +34,38 @@ const RegistroUser = () => {
         <div className="cardBoxtwo">
           <div className="card">
             <div>
+              <div className="numbers">Crear Nuevo Usuario</div>
+              <div>
+                <form onSubmit={handleSubmit(onSubmit)} >
+                  <div className="mb-3">
+                    <label htmlFor="InputNameUsuario" className="form-label">Nombre de Usuario</label>
+                    <input type="text" className="form-control" id="InputNameUsuario" maxLength={30} {...register("userName", { required: true })} required></input>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" maxLength={30} {...register("email", { required: true })} required></input>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="InputPassword1" className="form-label">Contraseña</label>
+                    <input type="password" className="form-control" id="InputPassword1" maxLength={30} {...register("password", { required: true })} required></input>
+                  </div>
+                  <label className="form-label">Tipo de Perfil</label>
+                  <select className="form-select mb-4" aria-label="Default select example" {...register("typeProfile", { required: true })} required>
+                    <option value="Vendedor">Vendedor</option>
+                    <option value="Administrador">Administrador</option>
+                  </select>
+                  <button type="submit" className="btn btn-primary">Guardar</button>
+                </form>
+              </div>
+
+            </div>
+
+
+          </div>
+          <div className="card">
+            <div>
               <h1 className="numbers mb-4">Lista de Usuarios</h1>
-              <table class="table">
+              <table className="table">
                 <thead>
                   <tr>
                     <th scope="col">#</th>
@@ -36,35 +89,49 @@ const RegistroUser = () => {
                   </tr>
                   <tr>
                     <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
+                    <td >Larry the Bird</td>
                     <td>@twitter</td>
                   </tr>
                 </tbody>
               </table>
             </div>
-
           </div>
           <div className="card">
             <div>
-              <div className="numbers">Crear Nuevo Usuario</div>
-              <div>
-                <form>
-                  <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                  </div>
-                  <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1"></input>
-                  </div>
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-              </div>
-
+              <h1 className="numbers mb-4">Perfil de Usuario Seleccionado</h1>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td >Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
+
       </div>
     </div>
   )
