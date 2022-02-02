d95432e3fd50e902947a26af7c2a0794a211fa40
jcarrizo <jcarrizo15@gmail.com>
2022-02-02 16:15:21 -0300

    se agrego la funcion de eliminar

--- src/pages/dashboard/registroUser/registroUser.js
+++ src/pages/dashboard/registroUser/registroUser.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import Sidebar from '../../../components/sidebar/sidebar';
@@ -6,8 +6,24 @@ import '../registroUser/registroUser.css'
 
 const RegistroUser = () => {
   const { register, handleSubmit } = useForm();
+  const [datosUsuarios, setdatosUsuarios] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/user/allUsers', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        setdatosUsuarios(data);
+      })
+      .catch(err => console.error(err))
+  }, []);
+
   const onSubmit = data => {
-    console.log(data)
     fetch('http://localhost:8000/api/user', {
       method: 'POST',
       body: JSON.stringify(data),
@@ -18,14 +34,43 @@ const RegistroUser = () => {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data)
         toast("Se cargo el perfil correctamente", {
           type: "success",
           autoClose: 3000,
         });
+        // window.location.reload(true)
       })
       .catch(err => console.error(err))
   };
+
+
+  const EliminarUsuario = (id) => {
+    console.log(id)
+    let data = {
+      id: id
+    }
+
+    if (window.confirm("¿Está seguro que desea eliminar el paciente?")) {
+      fetch('http://localhost:8000/api/user/deleteUser', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          toast("Se elimino el perfil correctamente", {
+            type: "success",
+            autoClose: 3000,
+          });
+          // window.location.reload(true)
+        })
+        .catch(err => console.error(err))
+    }
+
+  }
   return (
     <div>
       <Sidebar />
@@ -49,7 +94,7 @@ const RegistroUser = () => {
                     <label htmlFor="InputPassword1" className="form-label">Contraseña</label>
                     <input type="password" className="form-control" id="InputPassword1" maxLength={30} {...register("password", { required: true })} required></input>
                   </div>
-                  <label className="form-label">Tipo de Perfil</label>
+                  <label className="form-label">Rango del Perfil</label>
                   <select className="form-select mb-4" aria-label="Default select example" {...register("typeProfile", { required: true })} required>
                     <option value="Vendedor">Vendedor</option>
                     <option value="Administrador">Administrador</option>
@@ -57,10 +102,7 @@ const RegistroUser = () => {
                   <button type="submit" className="btn btn-primary">Guardar</button>
                 </form>
               </div>
-
             </div>
-
-
           </div>
           <div className="card">
             <div>
@@ -68,30 +110,22 @@ const RegistroUser = () => {
               <table className="table">
                 <thead>
                   <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Usuario</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Rango</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td >Larry the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
+                <tbody className=''>
+                  {datosUsuarios.map((datos) => {
+                    console.log(datos)
+                    return (<tr>
+                      <td>{datos.userName}</td>
+                      <td>{datos.email}</td>
+                      <td>{datos.typeProfile}</td>
+                      <button type="button" className="btn btn-outline-danger mb-3" onClick={() => { EliminarUsuario(datos._id) }}>Eliminar</button>
+                    </tr>
+                    )
+                  })}
                 </tbody>
               </table>
             </div>
@@ -131,7 +165,6 @@ const RegistroUser = () => {
             </div>
           </div>
         </div>
-
       </div>
     </div>
   )
