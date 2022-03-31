bcd922ce54e4d98e4bf53c5214a33c1f2f57c730
jcarrizo <jcarrizo15@gmail.com>
2022-03-31 17:56:13 -0300

    arreglos

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Contact from "../pages/contact/contact";
-import Estadisticas from "../pages/dashboard/estadisticas/estadisticas";
-import Productos from "../pages/dashboard/productos/productos";
 import RegistroUser from "../pages/dashboard/registroUser/registroUser";
-import Home from "../pages/home/home";
 import Login from "../pages/login/login";
 
 const Rutas = () => {
@@ -12,10 +8,7 @@ const Rutas = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}></Route>
-        <Route path="/Contacto" element={<Contact />}></Route>
-        <Route path="/estadisticas" element={<Estadisticas />}></Route>
         <Route path="/usuarios" element={<RegistroUser />}></Route>
-        <Route path="/productos" element={<Productos />}></Route>
       </Routes>
     </BrowserRouter>
   );
